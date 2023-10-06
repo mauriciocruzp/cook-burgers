7d3b09189b635449aac9d13250021f46035d6719
Mauricio Cruz <mawcp7@gmail.com>
2023-10-06 08:30:37 -0600

    add dish prop and reset dishes

--- models/burgerPartsGenerator.go
+++ models/burgerPartsGenerator.go
@@ -15,9 +15,10 @@ type BurgerPartsGenerator struct {
 	beef        *canvas.Image
 	bottomBread *canvas.Image
 	status      bool
+	dish        *Dish
 }
 
-func NewBurgerPartsGenerator() *BurgerPartsGenerator {
+func NewBurgerPartsGenerator(dish *Dish) *BurgerPartsGenerator {
 	return &BurgerPartsGenerator{
 		topBread:    canvas.NewImageFromURI(storage.NewFileURI("./assets/burger/top_bread.png")),
 		ketchup:     canvas.NewImageFromURI(storage.NewFileURI("./assets/burger/ketchup.png")),
@@ -25,33 +26,48 @@ func NewBurgerPartsGenerator() *BurgerPartsGenerator {
 		beef:        canvas.NewImageFromURI(storage.NewFileURI("./assets/burger/beef.png")),
 		bottomBread: canvas.NewImageFromURI(storage.NewFileURI("./assets/burger/bottom_bread.png")),
 		status:      true,
+		dish:        dish,
 	}
 }
 
 func (b *BurgerPartsGenerator) Run() {
 	for b.status {
+		b.ResetItem(b.bottomBread)
 		b.bottomBread.Show()
 		go b.CollapseItem(b.bottomBread)
 
-		time.Sleep(time.Second * 3)
+		time.Sleep(time.Second * 4)
 
+		b.ResetItem(b.ketchup)
 		b.ketchup.Show()
 		go b.CollapseItem(b.ketchup)
 
-		time.Sleep(time.Second * 3)
+		time.Sleep(time.Second * 4)
 
+		b.ResetItem(b.lettuce)
 		b.lettuce.Show()
 		go b.CollapseItem(b.lettuce)
-		time.Sleep(time.Second * 3)
+		time.Sleep(time.Second * 4)
 
+		b.ResetItem(b.beef)
 		b.beef.Show()
 		go b.CollapseItem(b.beef)
-		time.Sleep(time.Second * 3)
+		time.Sleep(time.Second * 4)
 
+		b.ResetItem(b.topBread)
 		b.topBread.Show()
 		go b.CollapseItem(b.topBread)
-		time.Sleep(time.Second * 3)
+		time.Sleep(time.Second * 6)
+		b.hideAllImages()
 	}
+}
+
+func (b *BurgerPartsGenerator) hideAllImages() {
+	b.bottomBread.Hide()
+	b.ketchup.Hide()
+	b.lettuce.Hide()
+	b.beef.Hide()
+	b.topBread.Hide()
 }
 
 func randPosition() fyne.Position {
@@ -62,22 +78,22 @@ func randPosition() fyne.Position {
 func (b *BurgerPartsGenerator) CollapseItem(image *canvas.Image) {
 	for b.status {
 		image.Move(fyne.NewPos(image.Position().X, image.Position().Y+5))
-		time.Sleep(time.Millisecond * 20)
+		time.Sleep(time.Millisecond * 40)
+		if image.Position().X-100 < b.dish.img.Position().X && image.Position().X+280 > b.dish.img.Position().X+180 && image.Position().Y == 490 {
+			b.dish.SetItemOnDish(image)
+			break
+		}
 	}
 }
 
-func (b *BurgerPartsGenerator) MoveItems() {
-	b.topBread.Move(randPosition())
-	b.ketchup.Move(randPosition())
-	b.lettuce.Move(randPosition())
-	b.beef.Move(randPosition())
-	b.bottomBread.Move(randPosition())
+func (b *BurgerPartsGenerator) ResetItem(image *canvas.Image) {
+	image.Move(randPosition())
 }
 
 func (b *BurgerPartsGenerator) GetTopBread() *canvas.Image {
 	topBreadImage := b.topBread
 	topBreadImage.Resize(fyne.NewSize(180, 30))
-	topBreadImage.Move(fyne.NewPos(340, 0))
+	topBreadImage.Move(randPosition())
 	topBreadImage.Hide()
 
 	return topBreadImage
@@ -86,7 +102,7 @@ func (b *BurgerPartsGenerator) GetTopBread() *canvas.Image {
 func (b *BurgerPartsGenerator) GetKetchup() *canvas.Image {
 	ketchupImage := b.ketchup
 	ketchupImage.Resize(fyne.NewSize(180, 30))
-	ketchupImage.Move(fyne.NewPos(40, 0))
+	ketchupImage.Move(randPosition())
 	ketchupImage.Hide()
 
 	return ketchupImage
@@ -95,7 +111,7 @@ func (b *BurgerPartsGenerator) GetKetchup() *canvas.Image {
 func (b *BurgerPartsGenerator) GetLettuce() *canvas.Image {
 	lettuceImage := b.lettuce
 	lettuceImage.Resize(fyne.NewSize(180, 30))
-	lettuceImage.Move(fyne.NewPos(480, 0))
+	lettuceImage.Move(randPosition())
 	lettuceImage.Hide()
 
 	return lettuceImage
@@ -104,7 +120,7 @@ func (b *BurgerPartsGenerator) GetLettuce() *canvas.Image {
 func (b *BurgerPartsGenerator) GetBeef() *canvas.Image {
 	beefImage := b.beef
 	beefImage.Resize(fyne.NewSize(180, 30))
-	beefImage.Move(fyne.NewPos(640, 0))
+	beefImage.Move(randPosition())
 	beefImage.Hide()
 
 	return beefImage
@@ -113,7 +129,7 @@ func (b *BurgerPartsGenerator) GetBeef() *canvas.Image {
 func (b *BurgerPartsGenerator) GetBottomBread() *canvas.Image {
 	bottomBreadImage := b.bottomBread
 	bottomBreadImage.Resize(fyne.NewSize(180, 30))
-	bottomBreadImage.Move(fyne.NewPos(200, 0))
+	bottomBreadImage.Move(randPosition())
 	bottomBreadImage.Hide()
 
 	return bottomBreadImage
