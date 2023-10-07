93b6f5ab2c73ffdff76026769c59408401b14985
Mauricio Cruz <mawcp7@gmail.com>
2023-10-07 12:37:03 -0600

    add add points counter

---
+++ scenes/pointsCounter.go
@@ -0,0 +1,46 @@
+package scenes
+
+import (
+	"cook_burgers/models"
+	"fmt"
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/widget"
+	"time"
+)
+
+type PointsCounter struct {
+	counterLabel *widget.Label
+	points       int
+	window       fyne.Window
+	dish         *models.Dish
+}
+
+func (pc *PointsCounter) NewPointsCounter(window fyne.Window, dish *models.Dish) *PointsCounter {
+	return &PointsCounter{
+		counterLabel: widget.NewLabel("Hamburguesas: 0"),
+		points:       0,
+		window:       window,
+		dish:         dish,
+	}
+}
+
+func (pc *PointsCounter) Run() {
+	for {
+		if len(pc.dish.GetItemsOnDish()) == 5 {
+			pc.UpdateCounter()
+			time.Sleep(time.Second * 4)
+		}
+	}
+}
+
+func (pc *PointsCounter) UpdateCounter() {
+	pc.points++
+
+	pc.counterLabel.SetText("Hamburguesas: " + fmt.Sprint(pc.points))
+
+	pc.window.Canvas().Refresh(pc.counterLabel)
+}
+
+func (pc *PointsCounter) GetCounterLabel() *widget.Label {
+	return pc.counterLabel
+}
